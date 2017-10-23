07fca79bd9839fa50674196f07b412cac84349a9
Zhai Zhi Gang <36007881@qq.com>
2017-10-23 14:49:54 +0800

    1
    1

--- src/edu/xjtu/ee/dwfxpg/Main.java
+++ src/edu/xjtu/ee/dwfxpg/Main.java
@@ -8,19 +8,19 @@ import java.util.Iterator;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length > 0 && args[0].toLowerCase().equals("h")) {
-            System.out.println("show help");
-        } else if (args.length > 0 && args[0].equals("ZLCL")) {
+        if (args.length > 0 && args[0].toUpperCase().equals("ZLCL")) {
             CZLCL c = new CZLCL();
             c.Init();
             c.Calc();
             c.OutputScreen();
             c.OutToFile();
-        } else {
+        } else if (args.length > 0 && args[0].toUpperCase().equals("PQ")) {
             CPQ cpq = new CPQ(5, 5, 0.00001);
             cpq.init();       //数据初始化
             cpq.solve();
             cpq.output();
+        } else {
+            System.out.println("show help");
         }
     }
 
--- src/edu/xjtu/ee/fhnlpg/Main.java
+++ src/edu/xjtu/ee/fhnlpg/Main.java
@@ -3,13 +3,13 @@ package edu.xjtu.ee.fhnlpg;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        if (args.length > 0 && args[0].toLowerCase().equals("h")) {
-            System.out.println("show help");
-        } else {
+        if (args.length > 0 && args[0].toUpperCase().equals("RDWD")) {
             CRDWD cr = new CRDWD();
             cr.Init();
             cr.solve();
             cr.print();
+        } else {
+            System.out.println("show help");
         }
     }
 }
