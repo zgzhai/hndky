2ba61e6c0a7f901bd2b9c4394bdfc92fd007bcec
Zhai Zhi Gang <36007881@qq.com>
2018-05-11 11:01:03 +0800

    12
    123

--- src/edu/xjtu/ee/fhnlpg/Main.java
+++ src/edu/xjtu/ee/fhnlpg/Main.java
@@ -13,7 +13,26 @@ public class Main {
             c.init(6, 6, 6, 6, 0.2, 0.2, 60, 80, 0.7);
             c.solve(70, 85, 0.8);
             c.print();
-        } else {
+        } else if(args.length > 0 && args[0].toUpperCase().equals("LOAD1")){
+            HSLoad hs = new HSLoad();
+            //load1.m
+            hs.init(3, 2, 17, 100, 0.9, 1.2, 105, 120, 1);
+            hs.solve();
+            hs.print();
+        } else if(args.length > 0 && args[0].toUpperCase().equals("LOAD2")){
+            HSLoad hs = new HSLoad();
+            //load2.m
+            hs.init(3, 2, 17, 100, 1, 1.2, 105, 120, 10);
+            hs.solve();
+            hs.print();
+        }else if(args.length > 0 && args[0].toUpperCase().equals("LOAD3")){
+            Load3 hs = new Load3();
+            //load3.m
+            hs.init(3, 2, 9, 100, 1, 2, 115, 140, 100);
+            hs.solve();
+            hs.print();
+        }
+        else {
             System.out.println("                        .---.                                                            ");
             System.out.println("                        |   |                                                            ");
             System.out.println("                        '---'                         __.....__           __.....__      ");
